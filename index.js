76bdd5d502110988479b5f7fa69eb39a6273bedd
Dev <devinsullins@gmail.com>
2023-01-19 17:58:14 -0600

    Home Page Finished

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-// Carousel
+/***********
+	Carousel
+
+************/
 
 const carouselContainer = document.querySelector('.carousel-container');
 const listImageArea = carouselContainer.querySelector('.next-list');
@@ -7,19 +10,13 @@ const currentImage = carouselContainer.querySelector('.current-image');
 const arrowLeft = carouselContainer.querySelector('.arrow-left');
 const arrowRight = carouselContainer.querySelector('.arrow-right');
 
-
 function styleList() {
 	if (listImageArea.scrollWidth == listImageArea.offsetWidth){
 		listImageArea.style.justifyContent = 'center'
 	} else {
 		listImageArea.style.justifyContent = 'flex-start'
 	}
-	
-};
 
-
-for (var i = listImageArea.children.length; i >= 0; i--) {
-listImageArea.appendChild(listImageArea.children[Math.random() * i | 0]);
 };
 
 function goToRight() {
@@ -54,7 +51,7 @@ function changeCurrentImage (newImage) {
 		currentImage.classList.remove('fadeIn');
 	}, 500);
 	currentImage.attributes.src.value = this.attributes.src.value;
-	listOfImages.forEach(image => image.classList.remove('current-image-list'));
+	//listOfImages.forEach(image => image.classList.remove('current-image-list'));
 	listOfImages.forEach(function (image) {
 		image.classList.remove('current-image-list');
 	})
@@ -75,56 +72,14 @@ window.addEventListener('resize', function (e) {
     NodeList.prototype.forEach = Array.prototype.forEach;
 })();
 
-listOfImages.forEach(image => image.addEventListener('click', changeCurrentImage));
+//listOfImages.forEach(image => image.addEventListener('click', changeCurrentImage));
 listOfImages.forEach(function(image) {
 	image.addEventListener('click', changeCurrentImage);
 });
 
 
-for (let i = 0; i < 21; i++) {
-	const img = document.createElement('img');
-    img.src = `Images/work-[i]${items[i]}`;
-    const li = document.createElement('li');
-    li.appendChild(img);
-    document.getElementById('next-list').appendChild(li);
+
+var ul = document.querySelector('ul');
+for (var i = ul.children.length; i >= 0; i--) {
+    ul.appendChild(ul.children[Math.random() * i | 0]);
 }
-// Randomize Carousel
-
-function randomize() {
-	const ul = document.querySelector('.next-list');
-	const lis = Array.from(ul.querySelectorAll('.image-of-list'));
-	lis.sort(() => 0.5 - Math.random());
-	for (const li of lis) {
-	  ul.appendChild(li);
-	}
-  }
-
-window.onload = function() {
-randomize();
-}
-
-// New Randomize Function
-
-
-// // Nav Menu
-// var lastScrollTop = 0;
-
-// window.addEventListener("scroll", function() {
-//   var header = document.querySelector(".navbar");
-//   var navMenu = document.getElementById("nav-menu");
-//   var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-//   if (currentScroll > lastScrollTop) {
-//     header.classList.add("hidden");
-//     navMenu.classList.add("hidden");
-//   } else {
-//     header.classList.remove("hidden");
-//     navMenu.classList.add("hidden");
-//   }
-//   lastScrollTop = currentScroll;
-// });
-
-// document.getElementById("menu-button").addEventListener("click", function() {
-//   var navMenu = document.getElementById("nav-menu");
-//   navMenu.classList.remove("hidden");
-//   navMenu.classList.toggle("active");
-// });
